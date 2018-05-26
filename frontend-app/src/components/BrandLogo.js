5da7d4aac3acd9d7b67cddee5d63426dc1833adc
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-05-26 11:27:03 +0300

    change favicon directory

--- frontend-app/src/components/BrandLogo.js
+++ frontend-app/src/components/BrandLogo.js
@@ -8,8 +8,8 @@ export default class BrandLogo extends Component {
     render() {
         return(
             <a href={'/'}>
-                <Image src="./logo-black.png" className="brand-logo__header brand-logo__black" />
-                <Image src="./logo-white.png" className="brand-logo__header brand-logo__white" />
+                <Image src="./system/header/logo-black.png" className="brand-logo__header brand-logo__black" />
+                <Image src="./system/header/logo-white.png" className="brand-logo__header brand-logo__white" />
             </a>
         )
     }
